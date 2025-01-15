e883604b5167ee37a6a38642a65a1a33b0b5d394
IDGS-805-22001494 <22001494@alumnos.utleon.edu.mx>
2025-01-14 18:49:46 -0600

    tem6_funciones

---
+++ tem6_funciones.py
@@ -0,0 +1,30 @@
+import os
+
+def funcion1():
+    os.system("cls")
+    print("hola, soy funcion1")
+    num1=int(input("numero1:  "))
+    num2=int(input("numero2:  "))
+    res=num1+num2
+def funcion2():
+    print("hola, soy funcion2")
+
+def run():
+    os.system("cls")
+    print("menu opciones")
+    print("1: funcion1")
+    print("2: funcion2")
+    print("0: salir")
+    opcion=int(input("opcion:  "))
+    if opcion==1:
+        funcion1()
+    if opcion==2:
+        funcion2()
+if __name__=="__main__":
+    run()
+
+op=int(input("numeros:  "))
+if op==1:
+    funcion1()
+else:
+    funcion2()
--- tema5_for_range.py
+++ tema5_for_range.py
@@ -1,5 +1,13 @@
 # for(int i=;1<28;i++){}
 
+
+
+tem1=list(range(10))
+tem2=range(10)
+print(type(tem1))
+print(type(tem2))
+
+
 for i in range(1,10,2):
     print(i)
     
